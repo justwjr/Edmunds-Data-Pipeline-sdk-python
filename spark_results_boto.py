d35ba6bca8c48b4690d43e365a7f3e20035df7bd
Justin J. Wang <justwjr@ucla.edu>
2017-03-08 21:30:01 -0800

    update

--- spark_results_boto.py
+++ spark_results_boto.py
@@ -1,9 +1,17 @@
+"""
+    :file: spark_results_boto.py
+    :brief: This program grabs the latest verson of the .html file from EMR
+     and uploads it to S3 to update the website.
+"""
+
 import ssl
 import boto
 from boto.s3.connection import S3Connection
+import os
+
 
 def boto_upload_s3(html_file):
-    conn = S3Connection()
+    conn = S3Connection(host='s3.amazonaws.com')
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
     website_bucket = conn.get_bucket('edmundssparksubmitresults')
@@ -11,8 +19,20 @@ def boto_upload_s3(html_file):
     output_file.content_type = 'text/html'
     output_file.set_contents_from_string(html_file, policy='public-read')
 
+
 if __name__ == '__main__':
-	top = open("topVehicles.html", "r")
-	html_file = top.read()
-	boto_upload_s3(html_file)
+    # grab the latest version of "topVehicles.html" from EMR
+    # scp - i
+    # ~ / the_cloud.pem
+    # hadoop @ ec2 - 54 - 159 - 219 - 64.
+    # compute - 1.
+    # amazonaws.com: ~ / topVehicles.html
+    # ~ / OneDrive / Edmunds - Car - Data - Pipeline - sdk - python
+    os.system(
+        "scp -i ~/the_cloud.pem hadoop@ec2-54-159-219-64.compute-1.amazonaws.com:~/topVehicles.html ~/OneDrive/Edmunds-Car-Data-Pipeline-sdk-python")
+
+    top = open("topVehicles.html", "r")
+    html_file = top.read()
+    boto_upload_s3(html_file)
+
 top.close()
